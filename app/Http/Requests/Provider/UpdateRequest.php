c739e109e3261b7b7766d877aa451e38cec4ada5
Saymon017 <simon.ramirez7000@gmail.com>
2024-03-31 01:26:46 -0500

    updateProvider

--- app/Http/Requests/Provider/UpdateRequest.php
+++ app/Http/Requests/Provider/UpdateRequest.php
@@ -11,7 +11,7 @@ class UpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,50 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=>'required|string|max:255',
+
+            'email'=>'required|email|string|unique:providers, email,'.
+            $this->route('provider')->id. '|max:255',
+
+            'ruc_number'=>'required|string|min:10 |unique:providers, ruc_number,'.
+            $this->route('provider')->id. '|max:10',
+
+            'address'=>'nulleble|string|max:255',
+
+            'phone'=>'required|string|min:10|unique:providers, phone,'.
+            $this->route('provider')->id. '|max:10',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'=>'Este campo es requerido.',
+            'name.string'=>'El valor no es correcto.',
+            'name.max'=>'Solo se permiten 250 caracteres.',
+
+            'email.required'=>'Este campo es requerido.',
+            'email.email'=>'Ingrese un correo valiso.',
+            'email.string'=>'El valor no es correcto.',
+            'email.max'=>'Solo se permiten 255 caracteres.',
+            'email.unique'=>'Ya se encuentra registrado o el correo esta en uso.',
+
+            'ruc_number.required'=>'Este campo es requerido.',
+            'ruc_number.string'=>'El valor no es correcto.',
+            'ruc_number.max'=>'Solo se permiten 10 caracteres',
+            'ruc_number.min'=>'Solo se permiten 10 caracteres.',
+            'ruc_number.unique'=>'Ya se encuentra registrado.',
+
+            'address.max'=>'Solo se permiten 255 caracteres.',
+            'address.string'=>'El valor no es correcto.',
+
+            'phone.required'=>'Este campo es requerido.',
+            'phone.string'=>'El valor no es correcto.',
+            'phone.max'=>'Solo se permiten 10 caracteres',
+            'phone.min'=>'Solo se permiten 10 caracteres.',
+            'phone.unique'=>'El numero ya se encuentra registrado.',
+
+
         ];
     }
 }
